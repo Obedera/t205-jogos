aba0ae58c81c59341f69c5861549278ef8c909ec
Obede <obede.silva7@gmail.com>
2019-07-16 22:34:54 -0300

    Ajustei a função iniciar

--- jogoForca/script.js
+++ jogoForca/script.js
@@ -1,34 +1,34 @@
 let resposta;
 
 function lerTexto(tipo){
-    return prompt('Digite a '+tipo);
+    return prompt('Digite '+tipo);
 }
 
 function definirResposta(){
-    let palavra = lerTexto('resposta');
+    let palavra = lerTexto('a resposta');
     resposta = palavra.split('');
-    console.log(resposta);
+    for(let item=0;item<resposta.length;item++){
+        document.querySelector('div').innerHTML += `<input type="text" id="${item}" value="">`;
+        console.log(item);
+    }
 }
 
 
-
+let tentativas = 5;
+let resultado; 
 function iniciar(){
+    resultado = false;
+    let letra = lerTexto('uma letra da palavra');
     for (let i=0;i<resposta.length;i++){
-        let tentativas = 4;
-        let letra = lerTexto((i+1)+'º letra da palavra');
-        console.log(i);
-        if (letra != resposta[i]){
-            while(letra != resposta[i]){
-                alert('Você errou, tentativas restantes '+tentativas);
-                letra = lerTexto((i+1)+'º letra da palavra');
-                tentativas--;
-                if(tentativas==0){
-                    return alert('vc perdeu');
-                }
-            }
-        }
         if(letra == resposta[i]){
-            document.querySelector('input').value += letra;  
+            document.querySelectorAll('input')[i].value += letra;
+            resultado=true;  
         }
+    }
+    if(resultado==false){
+        tentativas--;
+    }
+    if(tentativas<=0){
+        return alert('vc perdeu');
     }
 }
