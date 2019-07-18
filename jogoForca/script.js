8878421f33b60969893064ae6928314474fed7b3
Obede <obede.silva7@gmail.com>
2019-07-18 20:44:33 -0300

    Dividir funções

--- jogoForca/script.js
+++ jogoForca/script.js
@@ -1,34 +1,45 @@
 let tentativas = 5;
 let resposta;
+let resultado; 
 
 function lerTexto(tipo){
     return prompt('Digite '+tipo);
 }
 
-// função definir resposta e reiniciar dados
-function definirResposta(){
-    document.querySelectorAll('aside')[2].innerHTML = '<button onclick="definirResposta()">Definir Resposta</button>';
+function vetoriar(valor){
+    return valor.split('');
+}
+
+function reiniciar(){
     tentativas=5;
+    document.querySelectorAll('aside')[2].innerHTML = '<button onclick="definirResposta()">Definir Resposta</button>';
     document.querySelector('h3').innerHTML = 'Letras já inseridas:';
     document.querySelector('aside p').innerHTML = `${tentativas}`;
     document.querySelector('img').src = 'img/forca5.png';
-    let palavra = lerTexto('a resposta');
-    let dica = lerTexto('uma dica');
-    document.querySelector('h2').innerHTML = `Dica: ${dica}`;
     document.querySelector('div').innerHTML = '';
-    // converter texto em lista
-    resposta = palavra.split('');
-    // fazer os quadradinhos
-    for(let item=0;item<resposta.length;item++){
+
+}
+function fazerQuadradinhos(vetor){
+    for(let item=0;item<vetor.length;item++){
         document.querySelector('div').innerHTML += `<input type="text" value="?" disabled>`;
         console.log(item);
     }
+
+}
+
+function definirResposta(){
+    reiniciar();
+    let palavra = lerTexto('a resposta');
+    resposta = vetoriar(palavra);
+    let dica = lerTexto('uma dica');
+    document.querySelector('h2').innerHTML = `Dica: ${dica}`;
+    fazerQuadradinhos(resposta);
     document.querySelectorAll('aside')[2].innerHTML += '<button onclick="iniciar()">Iniciar</button>';
 }
 
 
-let resultado; 
 function iniciar(){
+    document.querySelectorAll('button')[1].innerHTML = 'chutar';
     resultado = false;
     let letra = lerTexto('uma letra da palavra');
     document.querySelector('h3').innerHTML += ` ${letra}`;
@@ -53,6 +64,7 @@ function iniciar(){
         if (verificador.value != '?'){
             contador++;
             if(contador==resposta.length){
+                document.querySelectorAll('aside')[2].innerHTML = '<button onclick="definirResposta()">Definir Resposta</button>';
                 return alert('vc ganhou');
             }
         }
