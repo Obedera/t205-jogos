522e3671a5d5e69fdca451195b945857e4ad68b4
Obede <obede.silva7@gmail.com>
2019-07-20 15:26:36 -0300

    Virar cartas

--- jogoMemoria/script.js
+++ jogoMemoria/script.js
@@ -1,10 +1,45 @@
-cartas = [];
-for (let item of document.querySelectorAll('img')){
-    for(i = 0; i<document.querySelectorAll('img').length;i++){
-        cartas[i] = item.src;
+function pegarCartas(){
+    cartas = [];
+    for (i = 0; i<document.querySelectorAll('img').length;i++){
+        cartas[i] = document.querySelectorAll('img')[i].src;
+    }
+    return cartas;
+}
+
+function mandarCartas(lista){
+    for (i = 0; i<lista.length;i++){
+        document.querySelectorAll('img')[i].src = lista[i];
     }
 }
+
+function embaralhar(lista){
+    let valor;
+    let indice_aleatorio;
+    for (let i = lista.length - 1; i !== 0; i--) {
+        indice_aleatorio = Math.floor(Math.random() * i);
+        valor = lista[i];
+        lista[i] = lista[indice_aleatorio];
+        lista[indice_aleatorio] = valor;
+    }
+    mandarCartas(lista);
+}
+
+embaralhar(pegarCartas());
+cartas = pegarCartas();
+
 console.log(cartas);
 
 
+
+function virarCartas(lista){
+    cartasViradas = []
+    for (j=0; j<(lista.length); j++){
+        cartasViradas[j] = '';
+    }
+    mandarCartas(cartasViradas);
+}
+
+setTimeout(function (){
+    virarCartas(cartas);
+}, 3000);
 
